ed87d2afdb4024e976b397e360340c6201d7740a
あさぴっぴ1020 <88693257+Asapi1020@users.noreply.github.com>
2025-02-27 15:31:48 +0900

    clang-formatとcmakeのセットアップ

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # cpp-tutorials
+
 C++ tutorials for me
+
+## VSCode
+次の拡張機能をインストール
+- C/C++ Extension Pack
+- Clang-Format
+- Code Runner (optional)
+
+### ビルド
+- Ctrl + Shift + Pでパレットを開き、CMake: Buildを実行
+- または、cmake.exeにPATHを通せば、`cmake --build ./build/`
+
+### フォーマット
+- 拡張機能でファイル保存時のフォーマットが可能
+- あるいはclang-format.exeをPATHに通せば、`clang-format -i .\src\main.cpp\` で特定のファイルをフォーマット可能
--- todo-list/main.cpp
+++ todo-list/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <vector>
 #include <string>
+#include <vector>
 
 int main() {
 	std::cout << "Welcome to TODO list" << std::endl;
@@ -30,8 +30,7 @@ int main() {
 			case 2: {
 				if (todoList.empty()) {
 					std::cout << "No tasks to show" << std::endl;
-				}
-				else {
+				} else {
 					std::cout << "Tasks:" << std::endl;
 					for (int i = 0; i < todoList.size(); i++) {
 						std::cout << i + 1 << ". " << todoList[i] << std::endl;
@@ -42,15 +41,13 @@ int main() {
 			case 3: {
 				if (todoList.empty()) {
 					std::cout << "No tasks to delete" << std::endl;
-				}
-				else {
+				} else {
 					std::cout << "Enter the task number to delete: ";
 					int taskNumber;
 					std::cin >> taskNumber;
 					if (taskNumber < 1 || taskNumber > todoList.size()) {
 						std::cout << "Invalid task number" << std::endl;
-					}
-					else {
+					} else {
 						todoList.erase(todoList.begin() + taskNumber - 1);
 						std::cout << "Task deleted successfully" << std::endl;
 					}
